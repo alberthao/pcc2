10a7f351e6c8baee890f3d7b55482fc8a9f28253
Albert Hao <haohs@163.com>
2022-12-20 10:00:12 +0800

    chapter 7 no 4 exercises added.

---
+++ ex7-4.py
@@ -0,0 +1,10 @@
+#Author: Alberthao
+#Topic: Input
+
+ingridiant=""
+while ingridiant !='quit':
+        ingridiant =input("What is your favorite pizza flavor?")
+        message = f"We will add {ingridiant} to our pizza and I hope you will like it."
+        if ingridiant != 'quit':
+            print(message+"\n")
+        
