2e46dcccbfe0b8a6d4b5faada35b403bd1e8e1b0
Albert Hao <haohs@163.com>
2022-12-21 09:53:57 +0800

    ex7-4 modified a little, add \n to the end

--- ex7-4.py
+++ ex7-4.py
@@ -3,7 +3,7 @@
 
 ingridiant=""
 while ingridiant !='quit':
-        ingridiant =input("What is your favorite pizza flavor?")
+        ingridiant =input("What is your favorite pizza flavor?\n")
         message = f"We will add {ingridiant} to our pizza and I hope you will like it."
         if ingridiant != 'quit':
             print(message+"\n")
