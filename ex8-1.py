2b1f6dd002072f754d057f435a10c1917158ef68
Albert Hao <haohs@163.com>
2022-12-22 12:56:55 +0800

    ex8-2 added

---
+++ ex8-1.py
@@ -0,0 +1,8 @@
+#Author: Alberthao
+#Topic: Function
+
+def display_message(): #我们用def表示定义函数
+    """显示简单的问候语"""
+    print("In this new chapter I will learn Function.")
+
+display_message()
---
+++ ex8-2.py
@@ -0,0 +1,9 @@
+#Author: Alberthao
+#Topic: Function
+
+def favorite_book(title): #我们用def表示定义函数
+    """"""
+    print(f"One of my favorite books is {title}.")
+
+favorite_book('Alice in Wonderland')
+favorite_book("Charlotte's Web")
