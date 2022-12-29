7b67c2730a495c01c70d3be74cd72975687ae624
Albert Hao <haohs@163.com>
2022-12-29 16:48:27 +0800

    chapter 8 finished

---
+++ ex8-15.py
@@ -0,0 +1,12 @@
+#Author: Alberthao
+#Topic: Function
+
+# start from here
+import printing_functions 
+#import printing_functions as p
+
+unprinted_designs = ['phone case','robot pendant','dodecahedron']
+completed_models = []
+
+printing_functions.print_models(unprinted_designs, completed_models)
+#p.print_models(unprinted_designs, completed_models)
